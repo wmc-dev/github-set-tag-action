41fa25bbc812212eb5842325e589f3ac392e5c6d
Hammer, Florian <florian.hammer@de.zuken.com>
2020-06-23 05:54:45 +0200

    Improve tag removement

--- gittag.sh
+++ gittag.sh
@@ -3,7 +3,8 @@ set -e
 tag=$1
 
 if [[ $(git ls-remote --exit-code --tags origin $tag) ]]; then
-  git push --delete origin $tag
+  git tag --delete $tag
+  git push --delete origin :refs/tags/$tag
 fi
 git tag $tag
 git push origin --tags
