dd97a2ab6ce3c509edf70ab025e601d3f1a5760b
Hammer, Florian <florian.hammer@de.zuken.com>
2020-06-23 06:22:37 +0200

    Delete old tag

--- gittag.sh
+++ gittag.sh
@@ -2,8 +2,8 @@
 set -e
 tag=$1
 
-# if [[ $(git ls-remote --exit-code --tags origin $tag) ]]; then
-#   git push --delete origin $tag
-# fi
-git tag $tag -f
+if [[ $(git ls-remote --exit-code --tags origin $tag) ]]; then
+  git push --delete origin $tag
+fi
+git tag $tag
 git push origin --tags
